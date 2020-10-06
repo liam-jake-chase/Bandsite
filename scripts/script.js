000afab58c6348893a21252abcc98f1cc4981338
Josh McKever <josh.fusillo@gmail.com>
2020-10-06 14:18:10 -0400

    added to main githu repo for public deploy

---
+++ scripts/script.js
@@ -0,0 +1,102 @@
+ // API Key and URL variable
+
+ const apiKey = "2ba0a00c-8dbc-4e0f-a231-e06793c9594d";
+ const apiURL = "https://project-1-api.herokuapp.com/comments?api_key=";
+ 
+
+ // Insert comment function
+ const form = document.querySelector(".comments__form");
+
+ form.addEventListener("submit", function insertComment(event) {
+     event.preventDefault();
+
+     axios
+         .post(apiURL + apiKey, {
+            name: event.target.name.value,
+            comment: event.target.comment.value
+         })
+         .then(() => {
+            getResponse();
+         })
+            form.reset();
+         });
+
+// Get comment function
+ let getResponse = () => {
+     axios
+         .get(apiURL + apiKey)
+         .then(response => {
+             console.log(response)
+             displayComment(response.data.sort((a, b) => {
+                 return b.timestamp - a.timestamp
+             })
+            )
+        })        
+
+ };
+ getResponse();
+
+
+ 
+ function displayComment(listArray) {
+    let commentsContainer = document.querySelector(".comments__posted-list");
+    
+        commentsContainer.innerHTML="";
+
+     listArray.forEach(listArray => {
+
+         let commentsSection = document.createElement("div");
+         commentsContainer.appendChild(commentsSection);
+         commentsSection.className = "comments__section";
+
+         let circle = document.createElement("div");
+         commentsSection.appendChild(circle);
+         circle.className = "comments__circle";
+
+         let name = document.createElement("h4")
+         commentsSection.appendChild(name);
+         name.innerHTML = listArray.name;
+         name.className = "comments__name";
+
+         let arrayDate = new Date(listArray.timestamp);
+         let properDate = arrayDate
+             .getUTCMonth() + 1 + "/" +
+             arrayDate.getUTCDate() + "/" +
+             arrayDate.getUTCFullYear();
+
+         let date = document.createElement("p");
+         commentsSection.appendChild(date);
+         date.innerHTML = properDate;
+         date.className = "comments__date";
+
+         let textSection = document.createElement("div");
+         commentsContainer.appendChild(textSection);
+         textSection.className = "comments__text-wrapper";
+
+         let comments = document.createElement("p");
+         textSection.appendChild(comments);
+         comments.innerHTML = listArray.comment;
+         comments.className = " comments__text-comment";
+
+         let deleteButton = document.createElement("button");
+         textSection.appendChild(deleteButton);
+         deleteButton.innerHTML = "REMOVE"
+         deleteButton.className = " comments__delete-button";
+         deleteButton.id = listArray.id;         
+         deleteButton.addEventListener("click", (event) => {
+            let idNumber = event.target.id;
+            deleteComment(idNumber);
+         });
+
+     })
+ }
+
+ let deleteComment = (id) => {
+    axios
+       .delete("https://project-1-api.herokuapp.com/comments/" + id + "?api_key=" + apiKey)
+       .then(() => {
+           getResponse();
+       })
+}
+
+
---
+++ scripts/shows.js
@@ -0,0 +1,146 @@
+ // API Key and URL variable
+
+const APIkey = "2ba0a00c-8dbc-4e0f-a231-e06793c9594d";
+const APIurl = "https://project-1-api.herokuapp.com/showdates?api_key=";
+
+// Get shows function
+let getShows = () => {
+    axios
+        .get(APIurl + APIkey)
+        .then(response => {
+            console.log(response)
+            displayShows(response.data) 
+            displayShowsLarge(response.data)         
+        })
+                   
+
+};
+getShows();
+
+
+
+    // Grabbing all data from the objects for the shows tickets section
+    const showsContainer = document.querySelector(".shows__main-container");
+
+    function displayShows(listArray) {
+        
+        listArray.forEach(listArray => {
+
+        const dateContainer = document.createElement("div");
+        showsContainer.appendChild(dateContainer);
+        dateContainer.className = "shows__date";
+        
+        const dateHeader = document.createElement("h5");
+        dateHeader.innerText = "DATE";       
+        dateContainer.appendChild(dateHeader);  
+        dateContainer.className = "shows__date-header";      
+        
+        const dateText = document.createElement("p");            
+        dateContainer.appendChild(dateText);
+        dateText.innerText = listArray.date;
+        dateText.className = "shows__date-text";
+
+        const venueContainer = document.createElement("div");
+        showsContainer.appendChild(venueContainer);
+        venueContainer.className = "shows__venue";
+
+        const venueHeader = document.createElement("h5");
+        venueHeader.innerText = "VENUE";
+        venueContainer.appendChild(venueHeader);
+        venueHeader.className = "shows__venue-header";
+
+        const venueText = document.createElement("p"); 
+        venueText.innerText = listArray.place;           
+        venueContainer.appendChild(venueText);       
+        venueText.className = "shows__venue-text";
+
+        const locationContainer = document.createElement("div");
+        showsContainer.appendChild(locationContainer);
+        locationContainer.className = "shows__location";
+
+        const locationHeader = document.createElement("h5");
+        locationHeader.innerText = "LOCATION";
+        locationContainer.appendChild(locationHeader);
+        locationHeader.className = "shows__location-header";
+
+        const locationText = document.createElement("p");
+        locationContainer.appendChild(locationText);
+        locationText.innerText = listArray.location;
+        locationText.className = "shows__location-text";
+
+        let ticketButton = document.createElement("button");
+        ticketButton.innerText = "BUY TICKETS";
+        showsContainer.appendChild(ticketButton);
+        ticketButton.className = "shows__button";
+
+        let breakLine = document.createElement("div");
+        showsContainer.appendChild(breakLine);
+        breakLine.className = "shows__breakline";
+        
+        
+        
+    })
+
+    
+};
+
+
+
+        const showsContainerLarge = document.querySelector(".shows__main-container-large");
+
+        const headerContainer = document.createElement("div");
+        showsContainerLarge.appendChild(headerContainer);
+        headerContainer.className = "shows__header-container"; 
+        
+        const sectionContainer = document.createElement("div");
+        showsContainerLarge.appendChild(sectionContainer);
+        sectionContainer.className = "shows__section-container"; 
+
+        const dateHeader = document.createElement("h5");
+        dateHeader.innerText = "DATES";       
+        headerContainer.appendChild(dateHeader);  
+        dateHeader.className = "shows__date-header-large";  
+
+        const venueHeader = document.createElement("h5");
+        venueHeader.innerText = "VENUE";
+        headerContainer.appendChild(venueHeader);
+        venueHeader.className = "shows__venue-header-large";
+
+        const locationHeader = document.createElement("h5");
+        locationHeader.innerText = "LOCATION";
+        headerContainer.appendChild(locationHeader);
+        locationHeader.className = "shows__location-header-large";        
+
+        function displayShowsLarge(listArray) {
+        
+            listArray.forEach(listArray =>  {
+
+            const infoContainer = document.createElement("div");
+            sectionContainer.appendChild(infoContainer);
+            infoContainer.className = "shows__info-container";
+
+            const dateText = document.createElement("p");            
+            infoContainer.appendChild(dateText);
+            dateText.innerText = listArray.date;
+            dateText.className = "shows__date-text";
+
+            const venueText = document.createElement("p"); 
+            venueText.innerText = listArray.place;           
+            infoContainer.appendChild(venueText);       
+            venueText.className = "shows__venue-text";
+
+            const locationText = document.createElement("p");
+            infoContainer.appendChild(locationText);
+            locationText.innerText = listArray.location;
+            locationText.className = "shows__location-text";
+
+            let ticketButton = document.createElement("button");
+            ticketButton.innerText = "BUY TICKETS";
+            infoContainer.appendChild(ticketButton);
+            ticketButton.className = "shows__button";
+
+            
+        })
+
+    }; 
+
